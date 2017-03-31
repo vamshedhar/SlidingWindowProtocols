6bc8e27b93d8fdeae9a6c553ff14dc2624efe3d4
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-03-31 12:44:28 -0400

    feat(Receiver): write received data to a file

--- Receiver.java
+++ Receiver.java
@@ -9,10 +9,13 @@ class Receiver
 		DatagramSocket serverSocket = new DatagramSocket(9876);
 		byte[] receivedPacket = new byte[1024];
 		byte[] sendData = new byte[1024];
+		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
 		System.out.println("Server Started: Waiting for packets!!");
 
-		while(true)
+		boolean end = false;
+
+		while(!end)
 		{
 			DatagramPacket receivePacket = new DatagramPacket(receivedPacket, receivedPacket.length);
 			serverSocket.receive(receivePacket);
@@ -21,11 +24,13 @@ class Receiver
 
 			RDTPacket packet = new RDTPacket(receiveData);
 
+			end = packet.last;
+			
 			String sentence = new String(packet.data);
 			System.out.println("Received Packet: " + packet.seq);
 			System.out.println("Last Packet: " + packet.last);
 			System.out.println("");
-			System.out.println(sentence);
+			writer.write(sentence);
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			String capitalizedSentence = sentence.toUpperCase();
@@ -34,6 +39,11 @@ class Receiver
 			new DatagramPacket(sendData, sendData.length, IPAddress, port);
 			serverSocket.send(sendPacket);
 		}
+
+		writer.flush();
+		writer.close();
+
+		serverSocket.close();
 	}
 }
 
