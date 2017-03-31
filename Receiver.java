cb6a1180e7b5861b600fee3472aad40a2475fdc4
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-03-31 11:15:51 -0400

    feat(): add receive packet no.

--- Receiver.java
+++ Receiver.java
@@ -10,14 +10,15 @@ class Receiver
 		byte[] sendData = new byte[1024];
 
 		System.out.println("Server Started: Waiting for packets!!");
+
+		int packetCount = 0;
 		while(true)
 		{
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
-			System.out.println("RECEIVED BYTE DATA: " + receivePacket.getData());
 			String sentence = new String( receivePacket.getData());
-			System.out.println("RECEIVED: " + sentence);
-			System.out.println("Length:" + receiveData.length);
+			System.out.println("Received Packet: " + (++packetCount));
+			System.out.println(sentence);
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			String capitalizedSentence = sentence.toUpperCase();
--- Sender.java
+++ Sender.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 class Sender
 {
@@ -14,32 +15,66 @@ class Sender
 
 	public static int TIMEOUT = 10000000;
 
+	public static String filename = "inputfile.txt";
+
 	public static void main(String args[]) throws Exception
 	{
 
-		String data = "";
-        File file = new File('inputfile.txt');
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-        //  System.out.println(scanner.nextLine());
-        	data = data + scanner.nextLine();
-        }
+		System.out.println("Sending file: " + filename);
 
+		File file = new File(filename);
+		FileInputStream inputStream = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		inputStream.read(data);
+		inputStream.close();
 
-		BufferedReader inFromUser =
-		new BufferedReader(new InputStreamReader(System.in));
+		int FILE_SIZE = data.length;
+
+		System.out.println("FILE SIZE: " + FILE_SIZE + " bytes");
+
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
-		clientSocket.send(sendPacket);
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		clientSocket.receive(receivePacket);
-		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER:" + modifiedSentence);
+
+		int packetCount = 0;
+
+		while (true) {
+
+			int startByte = packetCount * MSS;
+			int endByte = (packetCount + 1) * MSS;
+
+			byte[] sendData = Arrays.copyOfRange(data, packetCount * MSS, endByte > FILE_SIZE ? FILE_SIZE : endByte);
+			byte[] receiveData = new byte[MSS];
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+			clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER (Packet " + (++packetCount) + "):" + modifiedSentence.length());
+			System.out.println("");
+			System.out.println("");
+
+			if (endByte > FILE_SIZE) {
+				break;
+			}
+		}
+
 		clientSocket.close();
+
+
+		// BufferedReader inFromUser =
+		// new BufferedReader(new InputStreamReader(System.in));
+		// DatagramSocket clientSocket = new DatagramSocket();
+		// InetAddress IPAddress = InetAddress.getByName("localhost");
+		// byte[] sendData = new byte[1024];
+		// byte[] receiveData = new byte[1024];
+		// String sentence = inFromUser.readLine();
+		// sendData = sentence.getBytes();
+		// DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+		// clientSocket.send(sendPacket);
+		// DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		// clientSocket.receive(receivePacket);
+		// String modifiedSentence = new String(receivePacket.getData());
+		// System.out.println("FROM SERVER:" + modifiedSentence);
+		// clientSocket.close();
 	}
 }
