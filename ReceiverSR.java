57d218608184cf44a40844bd191d0424d4c1a69e
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-04-01 18:57:59 -0400

    feat(Checksum): implementation of checksum error to SR

--- ReceiverSR.java
+++ ReceiverSR.java
@@ -11,10 +11,12 @@ class ReceiverSR
 
 	public static void main(String args[]) throws Exception
 	{
+		int bitsOfSqeunceNo = Sender.bitsOfSqeunceNo;
+		int MSS = Sender.MSS;
 
 		HashMap<Integer, RDTPacket> receivedPackets = new HashMap<Integer, RDTPacket>();
 		DatagramSocket serverSocket = new DatagramSocket(9876);
-		byte[] receivedPacket = new byte[500 + 4 + 1];
+		byte[] receivedPacket = new byte[16 + 1 + 4 + MSS];
 		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
 		System.out.println("Server Started: Waiting for packets!!");
@@ -34,43 +36,49 @@ class ReceiverSR
 			// Convert Packet data to packet object
 			RDTPacket packet = new RDTPacket(receiveData, receivePacket.getLength());
 
-			System.out.println("Received Segment " + packet.getSeqNo() + ";");
+			if (packet.isValidPacket()) {
+	
+				System.out.println("Received Segment " + packet.getSeqNo() + ";");
 
-			if(!receivedPackets.containsKey(packet.getSeqNo())){
-				receivedPackets.put(packet.getSeqNo(), packet);
+				if(!receivedPackets.containsKey(packet.getSeqNo())){
+					receivedPackets.put(packet.getSeqNo(), packet);
 
-				if (waitingForPacket != packet.getSeqNo()) {
-					System.out.println("Out of Order Segment Received; Stored to Buffer; Expecting " + waitingForPacket);
+					if (waitingForPacket != packet.getSeqNo()) {
+						System.out.println("Out of Order Segment Received; Stored to Buffer; Expecting " + waitingForPacket);
+					}
+				} else{
+					System.out.println("Discarded Segment " + packet.getSeqNo() + ": Duplicate Packet;");
 				}
+				
+				while(receivedPackets.containsKey(waitingForPacket)){
+					RDTPacket bufferedPacket = receivedPackets.get(waitingForPacket);
+					end = packet.getLast();
+					String text = new String(packet.getData());
+					writer.write(text);
+					System.out.println("Delivered Packet: " + waitingForPacket);
+					waitingForPacket++;
+				}
+
+				// Create and ACK Packet with the sequence number
+				RDTAck ackPacket = new RDTAck(packet.getSeqNo());
+				byte[] ackData = ackPacket.generatePacket();
+
+				Thread.sleep(1000);
+
+				DatagramPacket sendACK = new DatagramPacket(ackData, ackData.length, receivePacket.getAddress(), receivePacket.getPort());
+
+				if (Math.random() > LOST_ACK) {
+					serverSocket.send(sendACK);
+				} else{
+					System.out.println("Lost ACK");
+				}
+
+				System.out.println("ACK Sent: " + ackPacket.getSeqNo());
+				System.out.println("");
 			} else{
-				System.out.println("Discarded Segment " + packet.getSeqNo() + ": Duplicate Packet;");
-			}
-			
-			while(receivedPackets.containsKey(waitingForPacket)){
-				RDTPacket bufferedPacket = receivedPackets.get(waitingForPacket);
-				end = packet.getLast();
-				String text = new String(packet.getData());
-				writer.write(text);
-				System.out.println("Delivered Packet: " + waitingForPacket);
-				waitingForPacket++;
+				System.out.println("Discarded " + packet.getSeqNo() + "; Checksum Error;");
 			}
 
-			// Create and ACK Packet with the sequence number
-			RDTAck ackPacket = new RDTAck(packet.getSeqNo());
-			byte[] ackData = ackPacket.generatePacket();
-
-			Thread.sleep(1000);
-
-			DatagramPacket sendACK = new DatagramPacket(ackData, ackData.length, receivePacket.getAddress(), receivePacket.getPort());
-
-			if (Math.random() > LOST_ACK) {
-				serverSocket.send(sendACK);
-			} else{
-				System.out.println("Lost ACK");
-			}
-
-			System.out.println("ACK Sent: " + ackPacket.getSeqNo());
-			System.out.println("");
 		}
 
 		writer.flush();
