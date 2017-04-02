ddc79b3b33833c63b8065ea8c766d3722bfa4222
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-04-02 03:05:00 -0400

    feat(): move both protocols to one class and choose based on input

--- Receiver.java
+++ Receiver.java
@@ -4,17 +4,18 @@
  */
 import java.io.*;
 import java.net.*;
-import java.util.Arrays;
-import java.nio.charset.Charset;
+import java.util.*;
 
 class Receiver
 {
 
+	// ACK loss probability
 	public static double LOST_ACK = 0.05;
 
 	public static void main(String args[]) throws Exception
 	{
 
+		// Validate arguments 
 		if (args.length != 1) {
             System.out.println("Invalid Format!");
             System.out.println("Expected Format: java Receiver <portNum>");
@@ -22,6 +23,7 @@ class Receiver
         }
 
         int receiverPort;
+
         // check if bit length is a valid integer
 		try{
 			receiverPort = Integer.parseInt(args[0]);
@@ -30,8 +32,12 @@ class Receiver
 			return;
 		}
 
+		// Open Socket
 		DatagramSocket serverSocket = new DatagramSocket(receiverPort);
 
+		System.out.println("Server Started: Waiting for packets!!");
+
+		// Receive Protocol details from the Sender
 		byte[] inputData = new byte[1024];
 		DatagramPacket inputDataPacket = new DatagramPacket(inputData, inputData.length);
 		serverSocket.receive(inputDataPacket);
@@ -46,68 +52,157 @@ class Receiver
 		int WINDOW_SIZE = Integer.parseInt(inputDataArray[2].trim());
 		int MSS = Integer.parseInt(inputDataArray[3].trim());
 
-		System.out.println("Receiving Protocol: " + protocol);
-
-
+		// Total Sequence No's allowed
 		int lastSeqNo = (int) (Math.pow(2.0, (double) bitsOfSqeunceNo));
 
-		
-		byte[] receivedPacket = new byte[16 + 1 + 4 + MSS];
-		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+		System.out.println("***********************************");
+		System.out.println("Receiving Protocol - " + protocol);
+		System.out.println("***********************************");
 
-		System.out.println("Server Started: Waiting for packets!!");
 
-		boolean end = false;
+		if (protocol.equals("GBN")) {
+			byte[] receivedPacket = new byte[16 + 1 + 4 + MSS];
+			BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
-		int waitingForAck = 0;
+			boolean end = false;
 
-		while(!end)
-		{
+			int waitingForPacket = 0;
 
-			// Receive Packet
-			DatagramPacket receivePacket = new DatagramPacket(receivedPacket, receivedPacket.length);
-			serverSocket.receive(receivePacket);
-			byte[] receiveData = receivePacket.getData();
+			while(!end)
+			{
 
-			// Convert Packet data to packet object
-			RDTPacket packet = new RDTPacket(receiveData, receivePacket.getLength());
+				// Receive Packet
+				DatagramPacket receivePacket = new DatagramPacket(receivedPacket, receivedPacket.length);
+				serverSocket.receive(receivePacket);
+				byte[] receiveData = receivePacket.getData();
 
-			if (packet.isValidPacket()) {
-				if (waitingForAck % lastSeqNo == packet.getSeqNo()) {
-					System.out.println("Received Segment " + packet.getSeqNo() + ";");
-					end = packet.getLast();
-					String text = new String(packet.getData());
-					writer.write(text);
-					waitingForAck++;
+				// Convert Packet data to packet object
+				RDTPacket packet = new RDTPacket(receiveData, receivePacket.getLength());
+
+				if (packet.isValidPacket()) {
+					if (waitingForPacket % lastSeqNo == packet.getSeqNo()) {
+						System.out.println("Received Segment " + packet.getSeqNo() + ";");
+						end = packet.getLast();
+						String text = new String(packet.getData());
+						writer.write(text);
+						waitingForPacket++;
+					} else{
+						System.out.println("Discarded " + packet.getSeqNo() + "; Out of Order Segment Received; Expecting " + waitingForPacket % lastSeqNo);
+					}
 				} else{
-					System.out.println("Discarded " + packet.getSeqNo() + "; Out of Order Segment Received; Expecting " + waitingForAck % lastSeqNo);
+					System.out.println("Discarded " + packet.getSeqNo() + "; Checksum Error;");
 				}
-			} else{
-				System.out.println("Discarded " + packet.getSeqNo() + "; Checksum Error;");
+
+				// Create and ACK Packet with the sequence number
+				RDTAck ackPacket = new RDTAck((waitingForPacket - 1) % lastSeqNo);
+				byte[] ackData = ackPacket.generatePacket();
+
+				Thread.sleep(100);
+
+				DatagramPacket sendACK = new DatagramPacket(ackData, ackData.length, receivePacket.getAddress(), receivePacket.getPort());
+
+				if (Math.random() > LOST_ACK) {
+					serverSocket.send(sendACK);
+				} else{
+					System.out.println("Lost ACK");
+				}
+
+				System.out.println("ACK Sent: " + ackPacket.getSeqNo());
+				System.out.println("");
 			}
 
-			// Create and ACK Packet with the sequence number
-			RDTAck ackPacket = new RDTAck((waitingForAck - 1) % lastSeqNo);
-			byte[] ackData = ackPacket.generatePacket();
+			writer.flush();
+			writer.close();
 
-			Thread.sleep(100);
+			serverSocket.close();
+		} else if(protocol.equals("SR")){
+			HashMap<Integer, RDTPacket> receivedPackets = new HashMap<Integer, RDTPacket>();
+		
+			byte[] receivedPacket = new byte[16 + 1 + 4 + MSS];
+			BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
-			DatagramPacket sendACK = new DatagramPacket(ackData, ackData.length, receivePacket.getAddress(), receivePacket.getPort());
+			boolean end = false;
 
-			if (Math.random() > LOST_ACK) {
-				serverSocket.send(sendACK);
-			} else{
-				System.out.println("Lost ACK");
+			int waitingForPacket = 0;
+
+			Queue<Integer> WINDOW = new ArrayDeque<>();
+
+			for(int i = 0; i < WINDOW_SIZE; i++){
+				WINDOW.add(i);
 			}
 
-			System.out.println("ACK Sent: " + ackPacket.getSeqNo());
-			System.out.println("");
+			while(!end)
+			{
+
+				// Receive Packet
+				DatagramPacket receivePacket = new DatagramPacket(receivedPacket, receivedPacket.length);
+				serverSocket.receive(receivePacket);
+				byte[] receiveData = receivePacket.getData();
+
+				// Convert Packet data to packet object
+				RDTPacket packet = new RDTPacket(receiveData, receivePacket.getLength());
+
+				if (packet.isValidPacket()) {
+		
+					System.out.println("Received Segment " + packet.getSeqNo() + ";");
+
+					ArrayList<Integer> WINDOW_LIST = new ArrayList<Integer>(WINDOW);
+
+					int actualSeqNo = waitingForPacket + WINDOW_LIST.indexOf(packet.getSeqNo());
+
+					if(!receivedPackets.containsKey(actualSeqNo)){
+						receivedPackets.put(actualSeqNo, packet);
+
+						if (waitingForPacket != actualSeqNo) {
+							System.out.println("Out of Order Segment Received; Stored to Buffer; Expecting " + waitingForPacket % lastSeqNo);
+						}
+					} else{
+						System.out.println("Discarded Segment " + packet.getSeqNo() + ": Duplicate Packet;");
+					}
+					
+					while(receivedPackets.containsKey(waitingForPacket)){
+						RDTPacket bufferedPacket = receivedPackets.get(waitingForPacket);
+						end = bufferedPacket.getLast();
+						String text = new String(bufferedPacket.getData());
+						writer.write(text);
+						System.out.println("Delivered Packet: " + waitingForPacket % lastSeqNo);
+						WINDOW.add((waitingForPacket + WINDOW_SIZE) % lastSeqNo);
+						waitingForPacket++;
+						WINDOW.remove();
+					}
+
+					// Create and ACK Packet with the sequence number
+					RDTAck ackPacket = new RDTAck(packet.getSeqNo());
+					byte[] ackData = ackPacket.generatePacket();
+
+					Thread.sleep(100);
+
+					DatagramPacket sendACK = new DatagramPacket(ackData, ackData.length, receivePacket.getAddress(), receivePacket.getPort());
+
+					if (Math.random() > LOST_ACK) {
+						serverSocket.send(sendACK);
+					} else{
+						System.out.println("Lost ACK");
+					}
+
+					System.out.println("ACK Sent: " + ackPacket.getSeqNo());
+					System.out.println("");
+				} else{
+					System.out.println("Discarded " + packet.getSeqNo() + "; Checksum Error;");
+				}
+
+			}
+
+			writer.flush();
+			writer.close();
+
+			serverSocket.close();
+		} else{
+			serverSocket.close();
 		}
 
-		writer.flush();
-		writer.close();
-
-		serverSocket.close();
+		
+		
 	}
 }
 
--- Sender.java
+++ Sender.java
@@ -12,11 +12,13 @@ class Sender
 	// File thats transfered as packets
 	public static String transferFilename = "fulldata.txt";
 
+	// Packet Loss Probability
 	public static double LOST_PACKET = 0.1;
 
 	public static void main(String args[]) throws Exception
 	{
 
+		// Validate arguments 
 		if (args.length != 3) {
             System.out.println("Invalid Format!");
             System.out.println("Expected Format: java Sender <filename or filepath> <portNum> <packetCount>");
@@ -46,6 +48,8 @@ class Sender
 		}
 
 
+		// Read protocol details from the input file
+
 		BufferedReader reader = new BufferedReader(new FileReader(parametersFileName));
 		List<String> lines = new ArrayList<String>();
 
@@ -54,6 +58,7 @@ class Sender
 		while((line = reader.readLine()) != null) {
 		    lines.add(line.trim());
 		}
+
 		reader.close();
 
 		String protocol = lines.get(0);
@@ -66,28 +71,22 @@ class Sender
 
 		int MSS = Integer.parseInt(lines.get(3));
 
-		// Main Part
 
-		// Open send socket
+		// Create a Socket
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
 
-		String dataToReceiver = protocol + "," + bitsOfSqeunceNo + "," + WINDOW_SIZE + "," + MSS;
 
+		// Send protocol details from input file to Receiver
+		String dataToReceiver = protocol + "," + bitsOfSqeunceNo + "," + WINDOW_SIZE + "," + MSS;
 		byte[] inputData = dataToReceiver.getBytes();
 
-		// Send protocol details to receiver
 		DatagramPacket inputDataPacket = new DatagramPacket(inputData, inputData.length, IPAddress, receiverPort);
-
 		clientSocket.send(inputDataPacket);
 
-		// Thread.sleep(100000);
 
-		int lastSeqNo = (int) (Math.pow(2.0, (double) bitsOfSqeunceNo));
-
+		// Read data from file to transfer into bytes
 		System.out.println("Sending file: " + transferFilename);
-
-		// Read data from file into bytes
 		File file = new File(transferFilename);
 		FileInputStream inputStream = new FileInputStream(file);
 		byte[] data = new byte[(int) file.length()];
@@ -97,101 +96,231 @@ class Sender
 		int FILE_SIZE = data.length;
 		System.out.println("FILE SIZE: " + FILE_SIZE + " bytes");
 
-		int lastPacketNo = FILE_SIZE / MSS;
+		// Total Sequence No's allowed
+		int lastSeqNo = (int) (Math.pow(2.0, (double) bitsOfSqeunceNo));
 
-		boolean endOfFile = false;
+		// Switch according to the protocol
 
-		int nextPacket = 0;
+		System.out.println("***********************************");
+		System.out.println("Using Protocol - " + protocol);
+		System.out.println("***********************************");
 
-		int waitingForAck = 0;
+		System.out.println("");
 
-		long startTime = System.currentTimeMillis();
+		if (protocol.equals("GBN")) {
 
-		Queue<Integer> WINDOW = new ArrayDeque<>();
+			int lastPacketNo = FILE_SIZE / MSS;
 
-		for(int i = 0; i < WINDOW_SIZE; i++){
-			WINDOW.add(i);
-		}
+			boolean endOfFile = false;
 
-		// System.out.println(lastPacketNo);
+			int nextPacket = 0;
 
-		while (waitingForAck <= lastPacketNo) {
+			int waitingForAck = 0;
 
-			while(nextPacket - waitingForAck < WINDOW_SIZE && !endOfFile){
-				startTime = System.currentTimeMillis();
-				int startByte = nextPacket * MSS;
-				int endByte = (nextPacket + 1) * MSS;
+			long startTime = System.currentTimeMillis();
 
-				if (endByte > FILE_SIZE) {
-					System.out.println("End of File");
-					endOfFile = true;
-				}
+			Queue<Integer> WINDOW = new ArrayDeque<>();
 
-				byte[] partData = Arrays.copyOfRange(data, nextPacket * MSS, endByte > FILE_SIZE ? FILE_SIZE : endByte);
-
-				int sqeNo = nextPacket % lastSeqNo;
-
-				RDTPacket dataPacket = new RDTPacket(sqeNo, partData, endOfFile);
-
-				byte[] sendData = dataPacket.generatePacket();
-
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
-
-				if (Math.random() > LOST_PACKET) {
-					clientSocket.send(sendPacket);
-				} else{
-					System.out.print("Lost Packet: " + sqeNo + "; ");
-				}
-
-				System.out.println("Sending " + sqeNo + "; Packet Size: " + sendData.length);
-				nextPacket++;
+			for(int i = 0; i < WINDOW_SIZE; i++){
+				WINDOW.add(i);
 			}
 
+			while (waitingForAck <= lastPacketNo) {
 
-			byte[] receiveData = new byte[4];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				while(nextPacket - waitingForAck < WINDOW_SIZE && !endOfFile){
+					startTime = System.currentTimeMillis();
+					int startByte = nextPacket * MSS;
+					int endByte = (nextPacket + 1) * MSS;
 
-			try{
-				long TIMER = TIMEOUT - (System.currentTimeMillis() - startTime);
+					if (endByte > FILE_SIZE) {
+						System.out.println("End of File");
+						endOfFile = true;
+					}
 
-				if (TIMER < 0) {
-					throw new SocketTimeoutException();
+					byte[] partData = Arrays.copyOfRange(data, nextPacket * MSS, endByte > FILE_SIZE ? FILE_SIZE : endByte);
+
+					int sqeNo = nextPacket % lastSeqNo;
+
+					RDTPacket dataPacket = new RDTPacket(sqeNo, partData, endOfFile);
+
+					byte[] sendData = dataPacket.generatePacket();
+
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+
+					if (Math.random() > LOST_PACKET) {
+						clientSocket.send(sendPacket);
+					} else{
+						System.out.print("Lost Packet: " + sqeNo + "; ");
+					}
+
+					System.out.println("Sending " + sqeNo + "; Packet Size: " + sendData.length);
+					nextPacket++;
 				}
 
-				clientSocket.setSoTimeout((int) TIMER);
 
-				clientSocket.receive(receivePacket);
+				byte[] receiveData = new byte[4];
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-				RDTAck ackPacket = new RDTAck(receivePacket.getData());
-				Thread.sleep(100);
-				System.out.println("");
-				System.out.println("Received ACK: " + ackPacket.getSeqNo());
+				try{
+					long TIMER = TIMEOUT - (System.currentTimeMillis() - startTime);
 
-				if (WINDOW.contains(ackPacket.getSeqNo())){
-					while(ackPacket.getSeqNo() != WINDOW.poll()){
-						startTime = System.currentTimeMillis();
+					if (TIMER < 0) {
+						throw new SocketTimeoutException();
+					}
+
+					clientSocket.setSoTimeout((int) TIMER);
+
+					clientSocket.receive(receivePacket);
+
+					RDTAck ackPacket = new RDTAck(receivePacket.getData());
+					Thread.sleep(100);
+					System.out.println("");
+					System.out.println("Received ACK: " + ackPacket.getSeqNo());
+
+					if (WINDOW.contains(ackPacket.getSeqNo())){
+						while(ackPacket.getSeqNo() != WINDOW.poll()){
+							startTime = System.currentTimeMillis();
+							WINDOW.add((waitingForAck + WINDOW_SIZE) % lastSeqNo);
+							waitingForAck++;
+						}
 						WINDOW.add((waitingForAck + WINDOW_SIZE) % lastSeqNo);
 						waitingForAck++;
 					}
-					WINDOW.add((waitingForAck + WINDOW_SIZE) % lastSeqNo);
-					waitingForAck++;
+				} catch(SocketTimeoutException e){
+
+					String message = "Packet " + waitingForAck % lastSeqNo + ": Timer expired; Resending";
+
+					for(int i = waitingForAck; i < nextPacket; i++){
+
+						int sqeNo = i % lastSeqNo;
+
+						message += (" " + sqeNo);
+
+						int startByte = i * MSS;
+						int endByte = (i + 1) * MSS;
+
+						byte[] partData = Arrays.copyOfRange(data, startByte, endByte > FILE_SIZE ? FILE_SIZE : endByte);
+
+						RDTPacket dataPacket = new RDTPacket(sqeNo, partData, endByte > FILE_SIZE);
+
+						byte[] sendData = dataPacket.generatePacket();
+
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+
+						if (Math.random() > LOST_PACKET) {
+							clientSocket.send(sendPacket);
+						}else{
+							System.out.println("Lost Packet: " + sqeNo);
+						}
+					}
+
+					startTime = System.currentTimeMillis();
+
+					message += "; Timer started";
+
+					System.out.println(message);
+					System.out.println("");
 				}
-			} catch(SocketTimeoutException e){
+				
+			}
 
-				String message = "Packet " + waitingForAck % lastSeqNo + ": Timer expired; Resending";
+			clientSocket.close();
+		} else if(protocol.equals("SR")){
+			HashMap<Integer, Long> sentPacketTimers = new HashMap<Integer, Long>();
 
-				for(int i = waitingForAck; i < nextPacket; i++){
+			HashMap<Integer, RDTAck> receivedAcks = new HashMap<Integer, RDTAck>();
 
-					int sqeNo = i % lastSeqNo;
+			int lastPacketNo = FILE_SIZE / MSS;
 
-					message += (" " + sqeNo);
+			boolean endOfFile = false;
 
-					int startByte = i * MSS;
-					int endByte = (i + 1) * MSS;
+			int nextPacket = 0;
+
+			int waitingForAck = 0;
+
+			Queue<Integer> WINDOW = new ArrayDeque<>();
+
+			for(int i = 0; i < WINDOW_SIZE; i++){
+				WINDOW.add(i);
+			}
+
+			while (waitingForAck <= lastPacketNo) {
+
+				while(nextPacket - waitingForAck < WINDOW_SIZE && !endOfFile){
+
+					sentPacketTimers.put(nextPacket, System.currentTimeMillis());
+
+					int startByte = nextPacket * MSS;
+					int endByte = (nextPacket + 1) * MSS;
+
+					if (endByte > FILE_SIZE) {
+						System.out.println("End of File");
+						endOfFile = true;
+					}
+
+					byte[] partData = Arrays.copyOfRange(data, nextPacket * MSS, endByte > FILE_SIZE ? FILE_SIZE : endByte);
+
+					RDTPacket dataPacket = new RDTPacket(nextPacket % lastSeqNo, partData, endOfFile);
+
+					byte[] sendData = dataPacket.generatePacket();
+
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+
+					if (Math.random() > LOST_PACKET) {
+						clientSocket.send(sendPacket);
+					} else{
+						System.out.println("Lost Packet: " + nextPacket % lastSeqNo);
+					}
+
+					System.out.println("Sending " + nextPacket % lastSeqNo + "; Packet Size: " + sendData.length);
+
+					nextPacket++;
+				}
+
+
+				byte[] receiveData = new byte[4];
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+				try{
+					long TIMER = TIMEOUT - (System.currentTimeMillis() - sentPacketTimers.get(waitingForAck));
+
+					if (TIMER < 0) {
+						throw new SocketTimeoutException();
+					}
+
+					clientSocket.setSoTimeout((int) TIMER);
+
+					clientSocket.receive(receivePacket);
+
+					RDTAck ackPacket = new RDTAck(receivePacket.getData());
+
+					System.out.println("Received ACK: " + ackPacket.getSeqNo());
+
+					ArrayList<Integer> WINDOW_LIST = new ArrayList<Integer>(WINDOW);
+
+					int actualSeqNo = waitingForAck + WINDOW_LIST.indexOf(ackPacket.getSeqNo());
+
+					receivedAcks.put(actualSeqNo, ackPacket);
+
+					Thread.sleep(100);
+
+					while(receivedAcks.containsKey(waitingForAck)){
+						WINDOW.add((waitingForAck + WINDOW_SIZE) % lastSeqNo);
+						waitingForAck++;
+						WINDOW.remove();
+					}
+
+					System.out.println("");
+				} catch(SocketTimeoutException e){
+
+					System.out.println("Packet " + waitingForAck % lastSeqNo + ": Timer expired; Resending " + waitingForAck % lastSeqNo);
+
+					int startByte = waitingForAck * MSS;
+					int endByte = (waitingForAck + 1) * MSS;
 
 					byte[] partData = Arrays.copyOfRange(data, startByte, endByte > FILE_SIZE ? FILE_SIZE : endByte);
 
-					RDTPacket dataPacket = new RDTPacket(sqeNo, partData, endByte > FILE_SIZE);
+					RDTPacket dataPacket = new RDTPacket(waitingForAck % lastSeqNo, partData, endByte > FILE_SIZE);
 
 					byte[] sendData = dataPacket.generatePacket();
 
@@ -200,20 +329,19 @@ class Sender
 					if (Math.random() > LOST_PACKET) {
 						clientSocket.send(sendPacket);
 					}else{
-						System.out.println("Lost Packet: " + sqeNo);
+						System.out.println("Lost Packet: " + waitingForAck % lastSeqNo);
 					}
+
+					sentPacketTimers.put(waitingForAck, System.currentTimeMillis());
+
+					System.out.println("Timer Re-started");
 				}
-
-				startTime = System.currentTimeMillis();
-
-				message += "; Timer started";
-
-				System.out.println(message);
-				System.out.println("");
+				
 			}
-			
-		}
 
-		clientSocket.close();
+			clientSocket.close();
+		} else{
+			clientSocket.close();
+		}
 	}
 }
